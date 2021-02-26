f0cf9a21ced0e01907c2fb0ec5dc2c849f8bc2f1
narmeen-kh99 <narmeen.kh99@gmail.com>
2021-02-26 23:16:15 +0200

    version with edit

---
+++ CoronaTrackApp/Laboratory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoronaTrackApp
+{
+    //+++++++++++++++++++++++
+    class Laboratory
+    {
+        private int labid;
+        private int testid;
+        private long personid;
+        private int date;
+        private bool result;
+        //++++++
+        public Laboratory(int labid,int testid, long personid, string date,bool result)
+        {
+            this.labid = labid;
+            this.testid = testid;
+            this.personid = personid;
+            this.date = date;
+            this.result = result;
+
+        }
+        //+++++++
+    }
+}
