30a239346576f71b28e95d96f9447833b711877f
narmeen-kh99 <narmeen.kh99@gmail.com>
2021-02-27 03:53:22 +0200

    deleteTests

---
+++ CoronaTrackAppTests/LabTestTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoronaTrackAppTests
+{
+    [TestClass]
+    public class LabTestTests
+    {
+        [TestMethod]
+        public void Replace_Params_Success()
+        {
+           // var myinput = Person
+        }
+    }
+}
